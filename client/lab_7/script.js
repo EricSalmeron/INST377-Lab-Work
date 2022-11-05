97a5673136957b6ef467253f9da9019e3f3b086c
EricSalmeron <69407681+EricSalmeron@users.noreply.github.com>
2022-11-04 20:58:06 -0400

    sdd

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,7 +1,19 @@
+/* eslint-disable max-len */
+
+/*
+  Hook this script to index.html
+  by adding `<script src="script.js">` just before your closing `</body>` tag
+*/
+
+/*
+  ## Utility Functions
+    Under this comment place any utility functions you need - like an inclusive random number selector
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+*/
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
 function injectHTML(list) {
@@ -28,12 +40,10 @@ function injectHTML(list) {
   
     ## What to do in this function
       - Accept a list of restaurant objects
-      - using a .forEach method, inject a list element into your index.html for every elemen
+      - using a .forEach method, inject a list element into your index.html for every element in the list
       - Display the name of that restaurant and what category of food it is
   */
 }
-
-
 
 function processRestaurants(list) {
   console.log("fired restaurants list");
@@ -55,7 +65,7 @@ function processRestaurants(list) {
   
       ## What to do in this function:
   
-      - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also ve
+      - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
       - using a .map function on that range,
       - Make a list of 15 random restaurants from your list of 100 from your data request
       - Return only their name, category, and location
@@ -63,18 +73,10 @@ function processRestaurants(list) {
     */
 }
 
-function filterList(list, filterInputValue) {
-    return list.filter((item) => {
-      const lowerCaseName = item.name.toLowerCase();
-      const lowerCaseQuery = filterInputValue.toLowerCase();
-      return lowerCaseName.includes(lowerCaseQuery);
-    });
-  }
-
 async function mainEvent() {
   /*
       ## Main Event
-        Separating your main programming from your side functions will help you organize y
+        Separating your main programming from your side functions will help you organize your thoughts
         When you're not working in a heavily-commented "learning" file, this also is more legible
         If you separate your work, when one piece is complete, you can save it and trust it
     */
@@ -87,21 +89,21 @@ async function mainEvent() {
 
   /*
       Let's get some data from the API - it will take a second or two to load
-      This next line goes to the request for 'GET' in the file at /server/routes/foodService
+      This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
       It's at about line 27 - go have a look and see what we're retrieving and sending back.
      */
   const results = await fetch("/api/foodServicePG");
-  const arrayFromJson = await results.json();
+  const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
   /*
       Below this comment, we log out a table of all the results using "dot notation"
       An alternate notation would be "bracket notation" - arrayFromJson["data"]
       Dot notation is preferred in JS unless you have a good reason to use brackets
-      The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 
+      The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
     */
   console.table(arrayFromJson.data);
 
-  // in your browser console, try expanding this object to see what fields are availa
+  // in your browser console, try expanding this object to see what fields are available to work with
   // for example: arrayFromJson.data[0].name, etc
   console.log(arrayFromJson.data[0]);
 
@@ -111,33 +113,23 @@ async function mainEvent() {
   );
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) { return; }
-
-    let currentList =[];
+  if (arrayFromJson.data?.length > 0) {
     // the question mark in this means "if this is set at all"
     submit.style.display = "block"; // let's turn the submit button back on by setting it to display as a block when we have data available
 
     loadAnimation.classList.remove("lds-ellipsis");
     loadAnimation.classList.add("lds-ellipsis_hidden");
-    
-
-    form.addEventListener('input', (event) => {
-        console.log('input', event.target.value);
-        const newFilterList = filterList(arrayFromJson.data, event.target.value);
-        injectHTML(newFilterList);
-    });
-    
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-    // this is a synchronous event event, because we already did our async request above, a
+    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
     form.addEventListener("submit", (submitEvent) => {
-      // This is needed to stop our page from changing to a new URL even though it heard a
+      // This is needed to stop our page from changing to a new URL even though it heard a GET request
       submitEvent.preventDefault();
 
       // This constant will have the value of your 15-restaurant collection when it processes
-      currentList = processRestaurants(arrayFromJson.data);
-      console.log(currentList);
+      const restaurantList = processRestaurants(arrayFromJson.data);
+      console.log(restaurantList);
       // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(currentList);
+      injectHTML(restaurantList);
 
       // By separating the functions, we open the possibility of regenerating the list
       // without having to retrieve fresh data every time
@@ -151,4 +143,4 @@ async function mainEvent() {
     It's calling the 'mainEvent' function at line 57
     It runs first because the listener is set to when your HTML content has loaded
   */
-document.addEventListener("DOMContentLoaded", async () => mainEvent());
+document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
